a5bab0d06a15d04fd1b38dd7c6fd9ea6d4f6eadb
bogocles <bogocles@users.noreply.github.com>
2020-11-30 19:50:55 -0700

    added attribute support for ...
    * Routes
    * HTTP request methods

---
+++ src/Attributes/Route.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bogosoft\Http\Mvc\Attributes;
+
+use Attribute;
+
+/**
+ * Identifies an endpoint for an HTTP request.
+ *
+ * @package Bogosoft\Http\Mvc\Attributes
+ */
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
+class Route
+{
+    /**
+     * Create a new route attribute.
+     *
+     * @param string  $path A path to associate with a class or class method.
+     * @param ?string $name An optional name for the new path.
+     */
+    function __construct(
+        public string $path,
+        public ?string $name = null
+        )
+    {
+    }
+}
